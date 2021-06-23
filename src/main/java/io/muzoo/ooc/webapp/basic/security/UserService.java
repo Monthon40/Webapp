230893b05050b71f2de3acde2ecf1167757498fa
Admin <monthon40@hotmail.com>
2021-06-23 12:15:01 +0700

    -Previous Lecture 12 video -Added AbstractRoutableHttpServlet -Added Routable -Added User -Added UserService -Added ServerRouter -Added SecurityService -Modify Home, login, logout to make it easier to add class -Some minor changes for login and logout for their username checker in SecurityService

---
+++ src/main/java/io/muzoo/ooc/webapp/basic/security/UserService.java
@@ -0,0 +1,21 @@
+package io.muzoo.ooc.webapp.basic.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService {
+
+    private Map<String, User> users = new HashMap<>();
+    {
+        users.put("strickwar", new User("strickwar", "12345"));
+        users.put("admin", new User("admin","12345"));
+    }
+
+    public User findByUsername(String username) {
+        return users.get(username);
+    }
+
+    public boolean checkIfUserExists(String username){
+        return users.containsKey(username);
+    }
+}
