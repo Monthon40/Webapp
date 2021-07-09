de1dd906369bf92a3b820a8442fbc3a3b8d8b855
Admin <monthon40@hotmail.com>
2021-07-09 16:52:32 +0700

    Added error 404 to trap all non-existing path

--- src/main/java/io/muzoo/ooc/webapp/basic/Webapp.java
+++ src/main/java/io/muzoo/ooc/webapp/basic/Webapp.java
@@ -5,6 +5,8 @@ import io.muzoo.ooc.webapp.basic.service.UserService;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ErrorPage;
+
 import java.io.File;
 
 public class Webapp {
@@ -27,9 +29,13 @@ public class Webapp {
 
         try {
             Context ctx = tomcat.addWebapp("",doceBase.getAbsolutePath());
-
-//            ServletRouter servletRouter = new ServletRouter();
             servletRouter.init(ctx);
+
+            //customize error page for redirection
+            ErrorPage error404Page = new ErrorPage();
+            error404Page.setErrorCode(404);
+            error404Page.setLocation("/WEB-INF/error404.jsp");
+            ctx.addErrorPage(error404Page);
 
             tomcat.start();
             tomcat.getServer().await();
